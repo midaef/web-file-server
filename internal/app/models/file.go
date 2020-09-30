e5c52e884083078cbd49e0a3432d4870b787d8f6
Midaef <36952775+midaef@users.noreply.github.com>
2020-09-30 11:54:56 +0800

    update(models, file): add images struct

--- internal/app/models/file.go
+++ internal/app/models/file.go
@@ -5,4 +5,18 @@ type File struct {
 	Directory string
 	Name      string
 	Size      int64
+	Data      []byte
+	Icon      []byte
+}
+
+// MainImages ...
+type MainImages struct {
+	FileIcon   []byte
+	FolderIcon []byte
+}
+
+// HelpImages ...
+type HelpImages struct {
+	BackLinkIcon []byte
+	DownloadIcon []byte
 }
